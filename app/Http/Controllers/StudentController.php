429eb69fb2a36c65d7d53e22c91246b827f0cae4
Samiullah <samikhan862@outlook.com>
2025-07-16 13:04:58 +0500

    update public

---
+++ app/Http/Controllers/StudentController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class StudentController extends Controller
+{
+    //
+    function create(){
+        return view('student.create');
+    }
+    function index(){
+        return view('student.create');
+    }
+}
