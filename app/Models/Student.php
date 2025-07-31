a84ec1a5dce18042db715e3804b622b66fb17dc6
Samiullah <samikhan862@outlook.com>
2025-07-31 11:53:02 +0500

    model

---
+++ app/Models/Student.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use mysql_xdevapi\Table;
+
+class Student extends Model
+{
+    //
+    protected $table='my_students';
+}
