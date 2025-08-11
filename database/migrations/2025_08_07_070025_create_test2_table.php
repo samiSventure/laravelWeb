45ccace87df0b0d621ad1ddf9e427fbc6010f9e1
Samiullah <samikhan862@outlook.com>
2025-08-11 12:35:10 +0500

    relation

---
+++ database/migrations/2025_08_07_070025_create_test2_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('Student2', function (Blueprint $table) {
+            $table->id();
+            $table->string('Name')->nullable();
+            $table->string('Email')->unique();
+            $table->string('password');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('test2');
+    }
+};
